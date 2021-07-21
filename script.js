8b78f77d6fc352fef46e08d8a07a434ea713b78a
Elithon Silva <elithonsilva@gmail.com>
2021-07-21 15:59:06 -0300

    Requisito 03

---
+++ script.js
@@ -0,0 +1,13 @@
+const trybeLogin = document.querySelector('#login');
+const trybeSenha = document.querySelector('#password');
+const buttonLogin = document.querySelector('#buttonLogin');
+
+function verificarLogin () {
+  if (trybeLogin.value === 'trybe@teste.com' || trybeSenha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+}
+
+buttonLogin.addEventListener('click', verificarLogin);
